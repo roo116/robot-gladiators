b042b9cec4ccba7227884cd43b6b4d08445cb9b3
roo <roo116@nc.rr.com>
2022-04-24 15:04:48 -0400

    added some console.log messages to js file along with window prompt and window alert code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
 window.alert("This is an alert! Javascript is running!");
+
+// fight function
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//get robot's name
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName)
+
+console.log("This holds a string, good for leaving yourself a message.")
+//This will do math and log 20
+console.log(10 + 10)
+//what is this?
+console.log("Our robot's name is " + playerName)
+// fight();
