9b0dd9f8d3ea1fadf6ef28287abb4d4f1f0a1900
roo <roo116@nc.rr.com>
2022-04-26 02:43:39 -0400

    finished up the while loop and fight function and seems to be working now

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,58 +11,69 @@ var enemyAttack = 12;
 
 // creating a fight function expression
 var fight = function (enemyName) {
-    while (enemyHealth > 0) {
+    while (playerHealth > 0 && enemyHealth > 0) {
         window.alert("Welcome to Robot Gladiators!");
+
+        // ask player if they want to fight or run
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose.");
-        console.log(promptFight)
-        // if player chooses to fight then fight
-        if (promptFight === "FIGHT" || promptFight === "fight") {
 
-            // Subtract the the value of playerAttack from the value of enemyHealth and use that result to update the value in enemyHealth var
-            enemyHealth = enemyHealth - playerAttack;
-            // log result
-            console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-            //  check enemy health
-            if (enemyHealth <= 0) {
-                window.alert(enemyName + " had died!");
-            } else {
-                window.alert(enemyName + " still has " + enemyHealth + " left!");
-            }
-
-            // subtract enemyAttack from playerHealth, update playerHealth var
-            playerHealth = playerHealth - enemyAttack;
-            // log result
-            console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
-            // check player's health
-            if (playerHealth <= 0) {
-                window.alert(playerName + " has died!")
-            } else {
-                window.alert(playerName + " still has " + playerHealth + " left!")
-            }
-            //end fight section
-
-            //if play skips
-        } else if (promptFight === "SKIP" || promptFight === "skip") {
+        //if player skips
+        if (promptFight === "SKIP" || promptFight === "skip") {
             var confirmSkip = window.confirm("are you sure you want to skip the fight?  It's gonna cost you.");
+
             //if yes TRUE, leave the fight
             if (confirmSkip) {
-                window.alert(playerName + " has decided to skip this fight.  Goodbye!")
+                window.alert(playerName + " has decided to skip this fight.  Goodbye!");
                 //subtract the cost for skipping
-                playerMoney = playerMoney - 2
-            }
-            // if no FALSE ask the question again
-            else {
-                fight()
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
             }
         }
-    }
-}
+
+
+        // // if player chooses to fight then fight
+        // if (promptFight === "FIGHT" || promptFight === "fight") {
+
+        // Subtract the the value of playerAttack from the value of enemyHealth and use that result to update the value in enemyHealth var
+        enemyHealth = enemyHealth - playerAttack;
+        // log result
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+       
+        //  check enemy health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " had died!");
+
+        // award player money for kicking a$$
+        playerMoney = playerMoney + 20;
+        //leave while loop cause the enemy is day-ed
+        break;
+        } else {
+            window.alert(enemyName + " still has " + enemyHealth + " left!");
+        }
+
+        // subtract enemyAttack from playerHealth, update playerHealth var
+        playerHealth = playerHealth - enemyAttack;
+        // log result
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        
+        // check player's health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+            // leave the loop if the playa is day-ed
+            break;
+        } else {
+            window.alert(playerName + " still has " + playerHealth + " left!");
+        }
+    }  // end while loop
+} //end fight function
+
 
 
 // FIGHT!!!!
 for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50
-    // debugger;
+    debugger;
     fight(pickedEnemyName);
 }
